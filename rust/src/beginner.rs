fa6400ecfe01ca35318ad621c6c49042beca03eb
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-02-01 19:54:26 -0300

    Organized with modules.

---
+++ rust/src/beginner.rs
@@ -0,0 +1,13 @@
+/* Begineer exercises. */
+
+use std::io;
+
+// Output "Hello World!".
+pub fn e1000() -> String { String::from("Hello World!") }
+
+
+/* "Extremely basic": Read two integers A and B, add
+ * them assigning the result to the variable X; print
+ * the result in the format "X = **". */
+//fn e1001() -> String {
+//}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,12 @@
-/* Each function is about one exercise. */
+/* Beecrowd separates the exercises by categorys.
+ * In this project, each category has its own module item,
+ * which is imported to this very item to be tested.
+ * Each function is a solution to a problem. */
 
-// Output "Hello World!".
-fn e1000() -> String { String::from("Hello World!") }
+use std::io;
+
+use crate::beginner::*;
+mod beginner;
 
 #[cfg(test)]
 mod tests {
